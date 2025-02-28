fbbf451d8ceca302d3fca20f719d5ffda91c8aa8
Maria Eduarda <151252340+devdudinha@users.noreply.github.com>
2025-02-27 23:44:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const menuContainer = document.querySelector(".menu-container");
     const menuToggle = document.getElementById("menu-toggle");
-    const sidebar = document.getElementById("sidebar");
+    const menuBackdrop = document.querySelector(".menu-backdrop");
 
     if (menuToggle) { // Adiciona verificação para evitar erros
+
+        // Adiciona o evento de abrir o menu
         menuToggle.addEventListener("click", function () {
-            sidebar.classList.toggle("active");
+            menuContainer.classList.toggle("active");
+        });
+
+        // Adiciona o evento de fechar o menu (ao apertar do lado de fora do menu)
+        menuBackdrop.addEventListener("click", function () {
+            menuContainer.classList.remove("active");
         });
     }
+
+    // Adiciona o evento de fechar o menu ao clicar em um botão do menu lateral
+    const sidebarButtons = document.querySelectorAll(".sidebar a");
+    
+    sidebarButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            menuContainer.classList.remove("active");
+        });
+    });
 });
